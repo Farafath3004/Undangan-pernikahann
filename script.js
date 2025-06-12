a8b3a44289306d718e6b7b37514a53a02d57393c
Farafath3004 <145646855+Farafath3004@users.noreply.github.com>
2025-06-12 13:04:51 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function showMessage() {
+  alert("Terima kasih! Kehadiran Anda sangat berarti bagi kami. ❤️");
+}
+
+function showInvitation() {
+  document.getElementById("cover-page").style.display = "none";
+  document.getElementById("invitation-page").style.display = "block";
+}
